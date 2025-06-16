ec401bc326d642cfd5792844ca488d33de8d6498
Iago Martins <iagomartinsgamedesign@gmail.com>
2025-06-16 15:56:07 -0300

    feat(userLogin): add ability to execute login action.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryClient } from "@tanstack/react-query";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +12,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const queryClient = new QueryClient();
 
 export const metadata: Metadata = {
   title: "Create Next App",
---
+++ src/types/User.ts
@@ -0,0 +1,12 @@
+type User = {
+    id: number;
+    email: string;
+    password: string;
+    name: string;
+    role: string;
+    avatar: string;
+    creationAt: string;
+    updatedAt: string;
+};
+
+export default User;
